0444361d03c35810b1b224033b94c11ce69cde6d
Dan Shanahan <leftrightleft@github.com>
2022-01-11 23:14:34 +0000

    converting to a template

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # Container image that runs your code
 FROM python:3.10
 
-COPY markdown-faq/main.py /tmp/
-
-CMD ["python", "/tmp/main.py"]
+COPY markdown-faq/ /src/
+WORKDIR /src/
+CMD ["python", "main.py"]
--- markdown-faq/main.py
+++ markdown-faq/main.py
@@ -1,38 +1,51 @@
 #!/usr/bin/python
 
 import os
-import json
+from string import Template
+# import json
  
 # printing environment variables
 # print("printing env vars")
 # context = json.dumps(os.environ)
-body = '''---
-title: {}
-version: 1.0
-datetime:
-  draft: {}
-  published: {}
-catagories:
-  - Internal
-  - External
-owners:
-  - GeekMasher
-  - alonahlobina
-  - adityasharad
----
-## {}
-{}
-'''.format(os.environ['title'],
-        os.environ['created_at'], 
-        os.environ['updated_at'], 
-        os.environ['title'], 
-        os.environ['body'])
+# body = '''---
+# title: {}
+# version: 1.0
+# datetime:
+#   draft: {}
+#   published: {}
+# catagories:
+#   - Internal
+#   - External
+# owners:
+#   - GeekMasher
+#   - alonahlobina
+#   - adityasharad
+# ---
+# ## {}
+# {}
+# '''.format(os.environ['title'],
+#         os.environ['created_at'], 
+#         os.environ['updated_at'], 
+#         os.environ['title'], 
+#         os.environ['body'])
 # print('action', os.environ['action'])
 # print('created', os.environ['created_at'])
 # print('updated', os.environ['updated_at'])
 # print('login', os.environ['login'])
 # print('body', os.environ['body'])
+# fields = {
+#     'title':'testingtitle',
+#     'created':'qweqwe',
+#     'updated':'234234234',
+#     'body':'this is the body',
+#     'creator':'creator text'
+#     }
+
+with open('template.txt', 'r') as f:
+    src = Template(f.read())
+    result = src.substitute(os.environ)
 
 f = open(os.environ['title'] + ".md", "w")
-f.write(body)
+
+f.write(result)
 f.close()
