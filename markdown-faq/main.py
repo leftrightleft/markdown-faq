8b820521d65714d94b848fa5e6bdd6db8ffeb02c
Dan Shanahan <leftrightleft@github.com>
2022-01-11 21:19:17 +0000

    use title

--- markdown-faq/main.py
+++ markdown-faq/main.py
@@ -8,16 +8,31 @@ import json
 # context = json.dumps(os.environ)
 body = '''
 ---
-created: {}
-updated: {}
+title: {}
+version: 1.0
+datetime:
+  draft: {}
+  published: {}
+catagories:
+  - Internal
+  - External
+owners:
+  - GeekMasher
+  - alonahlobina
+  - adityasharad
 ---
+## {}
 {}
-'''.format(os.environ['created_at'], os.environ['updated_at'], os.environ['body'])
-print('action', os.environ['action'])
-print('created', os.environ['created_at'])
-print('updated', os.environ['updated_at'])
-print('login', os.environ['login'])
-print('body', os.environ['body'])
+'''.format(os.environ['title'],
+        os.environ['created_at'], 
+        os.environ['updated_at'], 
+        os.environ['title'], 
+        os.environ['body'])
+# print('action', os.environ['action'])
+# print('created', os.environ['created_at'])
+# print('updated', os.environ['updated_at'])
+# print('login', os.environ['login'])
+# print('body', os.environ['body'])
 
 f = open("demofile2.md", "w")
 f.write(body)
