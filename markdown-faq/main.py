8d752ca8fae3eaeccf8e550938705eb0248ea728
Dan Shanahan <leftrightleft@github.com>
2022-01-11 01:17:23 +0000

    format body

--- markdown-faq/main.py
+++ markdown-faq/main.py
@@ -6,13 +6,14 @@ import json
 # printing environment variables
 # print("printing env vars")
 # context = json.dumps(os.environ)
-frontmatter = '''
+body = '''
 ---
 action: {}
 created: {}
 updated: {}
 ---
-'''
+{}
+'''.format(os.environ['created_at'], os.environ['updated_at'], os.environ['body'])
 print('action', os.environ['action'])
 print('created', os.environ['created_at'])
 print('updated', os.environ['updated_at'])
@@ -20,5 +21,5 @@ print('login', os.environ['login'])
 print('body', os.environ['body'])
 
 f = open("demofile2.md", "a")
-f.write(frontmatter, os.environ['body'])
+f.write(body)
 f.close()
