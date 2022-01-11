86cf399b3bf293f8a108307a48dcddc102240af2
Dan Shanahan <leftrightleft@github.com>
2022-01-11 01:14:10 +0000

    add frontmatter

--- markdown-faq/main.py
+++ markdown-faq/main.py
@@ -6,6 +6,13 @@ import json
 # printing environment variables
 # print("printing env vars")
 # context = json.dumps(os.environ)
+frontmatter = '''
+---
+action: {}
+created: {}
+updated: {}
+---
+'''
 print('action', os.environ['action'])
 print('created', os.environ['created_at'])
 print('updated', os.environ['updated_at'])
@@ -13,5 +20,5 @@ print('login', os.environ['login'])
 print('body', os.environ['body'])
 
 f = open("demofile2.md", "a")
-f.write(os.environ['body'])
+f.write(frontmatter, os.environ['body'])
 f.close()
