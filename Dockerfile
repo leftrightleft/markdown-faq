e603c1540e0478a317bab7f46b43677cde5fd691
Dan Shanahan <leftrightleft@github.com>
2022-01-12 00:50:14 +0000

    get tags working

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,8 @@ FROM python:3.10
 
 COPY markdown-faq/ /markdown-faq/
 
+# RUN pip install --no-cache-dir --upgrade pip && \
+#     pip install --no-cache-dir -r /markdown-faq/requirements.txt
+RUN pip install pyyaml
+
 CMD ["python", "/markdown-faq/main.py"]
--- markdown-faq/main.py
+++ markdown-faq/main.py
@@ -2,50 +2,25 @@
 
 import os
 from string import Template
-# import json
- 
-# printing environment variables
-# print("printing env vars")
-# context = json.dumps(os.environ)
-# body = '''---
-# title: {}
-# version: 1.0
-# datetime:
-#   draft: {}
-#   published: {}
-# catagories:
-#   - Internal
-#   - External
-# owners:
-#   - GeekMasher
-#   - alonahlobina
-#   - adityasharad
-# ---
-# ## {}
-# {}
-# '''.format(os.environ['title'],
-#         os.environ['created_at'], 
-#         os.environ['updated_at'], 
-#         os.environ['title'], 
-#         os.environ['body'])
-# print('action', os.environ['action'])
-# print('created', os.environ['created_at'])
-# print('updated', os.environ['updated_at'])
-# print('login', os.environ['login'])
-# print('body', os.environ['body'])
-# fields = {
-#     'title':'testingtitle',
-#     'created':'qweqwe',
-#     'updated':'234234234',
-#     'body':'this is the body',
-#     'creator':'creator text'
-#     }
+import yaml
 
-with open('/markdown-faq/template.txt', 'r') as f:
-    src = Template(f.read())
-    result = src.substitute(os.environ)
 
-f = open(os.environ['title'] + ".md", "w")
+def get_tags():
+    tags_string = os.environ['body'].split("### Tags\\n\\n",1)[1]
+    tags = tags_string.split(',')
+    tags = {'tags':[x.strip(' ') for x in tags]}
+    formatted_tags = yaml.dump(tags, default_flow_style=False)
+    return formatted_tags
 
-f.write(result)
-f.close()
+def main():
+    tags = get_tags()
+    with open('/markdown-faq/template.txt', 'r') as f:
+        src = Template(f.read())
+        result = src.substitute(os.environ, tags=tags)
+
+    f = open(os.environ['title'] + ".md", "w")
+    f.write(result)
+    f.close()
+
+if __name__ == '__main__':
+    main()
